c594893b920371b8ec53535a6ebf46432f9a688c
Arsh Patel <arshpatel2001@gmail.com>
2025-04-13 22:35:21 -0400

    Initial Commit

---
+++ Core.cs
@@ -0,0 +1,47 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using System.Reflection.Emit;
+using ScheduleOne.UI.Items;
+
+
+[assembly: MelonInfo(typeof(ScheduleOne_LargerMoneyStacks.LargerMoneyStacks), "ScheduleOne_LargerMoneyStacks", "1.0.0", "Apatel78", null)]
+[assembly: MelonGame("TVGS", "Schedule I")]
+
+namespace ScheduleOne_LargerMoneyStacks
+{
+    public class LargerMoneyStacks : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            var harmony = new HarmonyLib.Harmony("com.apatel.schedule1.xppatch");
+
+            harmony.Patch(
+                AccessTools.Method(typeof(ItemUIManager), "UpdateCashDragAmount"),
+                transpiler: new HarmonyMethod(typeof(LargerMoneyStacks), nameof(TranspilerPatch))
+            );
+            harmony.Patch(
+                AccessTools.Method(typeof(ItemUIManager), "StartDragCash"),
+                transpiler: new HarmonyMethod(typeof(LargerMoneyStacks), nameof(TranspilerPatch))
+            );
+            harmony.Patch(
+                AccessTools.Method(typeof(ItemUIManager), "EndCashDrag"),
+                transpiler: new HarmonyMethod(typeof(LargerMoneyStacks), nameof(TranspilerPatch))
+            );
+
+            MelonLogger.Msg("Schedule1 XP Patch initialized!");
+        }
+        static IEnumerable<CodeInstruction> TranspilerPatch(IEnumerable<CodeInstruction> instructions)
+        {
+            MelonLogger.Msg("Transpiler called");
+            foreach (var instruction in instructions)
+            {
+                MelonLogger.Msg($"Opcode: {instruction.opcode}, Operand: {instruction.operand}");
+                if (instruction.opcode == OpCodes.Ldc_R4 && (float)instruction.operand == 1000f)
+                    yield return new CodeInstruction(OpCodes.Ldc_R4, (float)99999);
+                else
+                    yield return instruction;
+            }
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# SceduleI_LargerMoneyStacks
-Schedule I mod for changing money stack size
+# ScheduleI_LargerMoneyStacks
+A money stack adjusting mod for the game [Schedule 1](https://store.steampowered.com/app/1284220/Schedule_1/).
+
+## About
+ScheduleI_LargerMoneyStacks increases the money stack maximum size from 1000 to 99,999. This also changes the right click functionality of money, changing the default value from 1000 to 99,999.
+
+## Installation
+- Install MelonLoader (https://melonwiki.xyz/#)
+- Download the `LargerMoneyStacks.dll` file.
+- Place the `LargerMoneyStacks.dll` file in the Mods folder in your Schedule 1 installation folder.
+- The mod will automatically start with MelonLoader and will run in the background.
+
+
+## Download Link
+This mod is published under the open source MIT license:
+- LargerMoneyStacks GitHub (https://github.com/apatel78/ScheduleI_LargerMoneyStacks)
+- NexusMods (https://www.nexusmods.com/schedule1/mods/605)
